781229f89127be1613fac4a7cdf3cedaa0faa335
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-02-22 09:59:13 +0100

    added ; at the end of the lines

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Chiedi all'utente il suo nome, cognome, colore preferito e infine scrivi in pagina la loro concatenazione + un numero a caso
+
+/*
+x Chiedi il nome
+x Chiedi il cognome
+x Chiedi il colore preferito
+x Scrivi in pagina la loro concatenazione
+x Aggiungi alla concatenazione un numero a caso
+*/
+
+let nomeUtente = prompt("Come ti chiami?");
+
+let cognomeUtente = prompt("Qual è il tuo cognome?");
+
+let colorePrefUtente = prompt("Qual è il tuo colore preferito?");
+
+
+document.writeln(nomeUtente + cognomeUtente + colorePrefUtente + 21);
